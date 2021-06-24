bc7a9f350993e7d2bd3d806529f89b34cdb924e2
RAHATosman <rahatosman2018@gmail.com>
2021-06-24 23:24:34 +0600

    final push

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const express = require('express')
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
+const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
 app.use(express.json());
+
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
+
     res.send('Welcome to Gerez Server');
+
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pz3ns.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 client.connect(err => {
+
     const serviceCollection = client.db(`${process.env.DB_NAME}`).collection("services");
     const reviewCollection = client.db(`${process.env.DB_NAME}`).collection("reviews");
     const adminsCollection = client.db(`${process.env.DB_NAME}`).collection("admins");
     const orderCollection = client.db(`${process.env.DB_NAME}`).collection("orders");
-
-    app.get('/services', (req, res) => {
-        serviceCollection.find({})
-            .toArray((err, docs) => res.send(docs))
-    })
 
     app.get('/reviews', (req, res) => {
         if (req.query.email) {
@@ -34,6 +35,12 @@ client.connect(err => {
                 .toArray((err, docs) => res.send(docs[0]))
         }
         reviewCollection.find({})
+            .toArray((err, docs) => res.send(docs))
+    })
+
+    app.get('/services', (req, res) => {
+        
+        serviceCollection.find({})
             .toArray((err, docs) => res.send(docs))
     })
 
@@ -50,11 +57,6 @@ client.connect(err => {
             })
     })
 
-    app.get('/isAdmin', (req, res) => {
-        adminsCollection.find({ email: req.query.email })
-            .toArray((err, docs) => res.send(!!docs.length))
-    })
-
     app.post('/addService', (req, res) => {
         serviceCollection.insertOne(req.body)
             .then(result => res.send(!!result.insertedCount))
@@ -63,6 +65,11 @@ client.connect(err => {
     app.post('/addReview', (req, res) => {
         reviewCollection.insertOne(req.body)
             .then(result => res.send(!!result.insertedCount))
+    })
+
+    app.get('/isAdmin', (req, res) => {
+        adminsCollection.find({ email: req.query.email })
+            .toArray((err, docs) => res.send(!!docs.length))
     })
 
     app.post('/addAdmin', (req, res) => {
